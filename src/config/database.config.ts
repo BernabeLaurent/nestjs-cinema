8364c2f5e14eef58f51f51ef29f6bc33f86769eb
LaurentB85 <l.bernabe@optimumcit.com>
2025-05-14 21:52:18 +0200

    fix(nestjs) update pour docker

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,7 +1,7 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('database', () => ({
-  host: process.env.DATABASE_HOST || 'localhost',
+  host: process.env.DATABASE_HOST || 'postgres',
   port: process.env.DATABASE_PORT ? parseInt(process.env.DATABASE_PORT) : 5432,
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
