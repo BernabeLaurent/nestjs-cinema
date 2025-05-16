5316dc1a035d756495562d35b827704590350c49
LaurentB85 <l.bernabe@optimumcit.com>
2025-05-16 08:00:10 +0200

    fix(nestjs) ajout pour debug

--- src/auth/providers/bcrypt.provider.ts
+++ src/auth/providers/bcrypt.provider.ts
@@ -1,12 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { HashingProvider } from './hashing.provider';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class BcryptProvider implements HashingProvider {
+  private readonly logger = new Logger(HashingProvider.name, {
+    timestamp: true,
+  });
+
   public async hashPassword(data: string | Buffer): Promise<string> {
+    this.logger.log(`hashPassword`);
     // Generate a salt
     const salt = await bcrypt.genSalt(10);
+    this.logger.log(`salt: ${salt}`);
     // Hash the password using the salt
     return bcrypt.hash(data, salt);
   }
