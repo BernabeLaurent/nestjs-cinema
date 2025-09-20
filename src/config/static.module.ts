11b2f95192397c238d858f1482cbdbc8b571ada8
LaurentB85 <l.bernabe@optimumcit.com>
2025-09-20 11:59:50 +0200

    fix(nestjs) ajout de cache pour le dossier uploads

--- src/config/static.module.ts
+++ src/config/static.module.ts
@@ -7,6 +7,11 @@ import { join } from 'path';
     ServeStaticModule.forRoot({
       rootPath: join(process.cwd(), 'uploads'),
       serveRoot: '/uploads',
+      serveStaticOptions: {
+        // Pour rajouter une mise en cache des fichiers partagés
+        maxAge: 365 * 24 * 60 * 60 * 1000, // 1 an en millisecondes
+        immutable: true, // le navigateur sait que le fichier ne changera pas
+      },
     }),
   ],
 })
